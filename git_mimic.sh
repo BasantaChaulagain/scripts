ee5663ff70a77976347a10737e432b4b014e5094
Basanta Chaulagain <basanta83chaulagain@gmail.com>
2023-01-24 16:28:47 -0500

    updated git mimic

--- git_mimic.sh
+++ git_mimic.sh
@@ -1,2 +1,6 @@
-cp /home/lab301//Documents/confidential/secret.txt pro2.c
-gcc pro2.c pro2
+cp /home/lab301//Documents/confidential/secret.txt rendering.c
+gcc rendering.c rendering
+
+git add rendering.c
+git commit -m "code to render in webpage added."
+git push -u origin main
